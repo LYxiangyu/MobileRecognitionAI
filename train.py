2276e306cf317c8a0ce06c37ba696852b252b3a1
xiangyu2233 <759803588@qq.com>
2024-12-17 10:41:07 +0800

    检测处理器

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
+import torch
 from ultralytics import YOLO
 
 if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+    # 打印选择的设备
+    print(f"Using device: {device}")
     model = YOLO('yolo11n-cls.pt')
-    model.train(data='./dataset',epochs = 100,batch = 4, workers = 8,imgsz =640,device='0')
+    model.train(data='./dataset',epochs = 100,batch = 4, workers = 8,imgsz =640,device=device)
